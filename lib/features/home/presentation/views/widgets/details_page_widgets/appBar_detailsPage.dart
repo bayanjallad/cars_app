8f0533a5748c5d3d103d546a3aad611672c17b32
bayanjd <bayanjallad535@gmail.com>
2025-01-17 22:24:46 -0800

    init commit

--- lib/features/home/presentation/views/widgets/details_page_widgets/appBar_detailsPage.dart
+++ lib/features/home/presentation/views/widgets/details_page_widgets/appBar_detailsPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:share_plus/share_plus.dart';
 import '../../../../../../core/resources/color.dart';
 import '../../../../../../core/resources/string.dart';
 
@@ -8,6 +9,12 @@ class AppBarDetails extends StatelessWidget {
   const AppBarDetails({
     super.key,
   });
+
+  void _shareText() {
+    const String text = 'Check out this awesome Flutter app!';
+    const String url = 'https://flutter.dev';
+    Share.share('$text\n$url');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +27,12 @@ class AppBarDetails extends StatelessWidget {
             },
             icon: const Icon(Icons.arrow_back_ios_new_rounded)),
          Text(Strings.TESLA.tr, style:Theme.of(context).textTheme.displayLarge),
-         const Icon(Icons.share_outlined, color: blue),
+         InkWell(
+             onTap: (){
+
+               _shareText();
+             },
+             child: const Icon(Icons.share_outlined, color: blue)),
       ],
     );
   }
--- lib/features/home/presentation/views/widgets/details_page_widgets/detailsPrice.dart
+++ lib/features/home/presentation/views/widgets/details_page_widgets/detailsPrice.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../../../../../../config/responsive.dart';
 import '../../../../../../core/resources/color.dart';
 import '../../../../../../core/resources/string.dart';
 import '../../../../../../core/resources/style.dart';
 import '../../../../../../core/widgets/custom_button.dart';
 
+
+
+final String phoneNumber = "0939281171";
+
+Future<void> makePhoneCall(String number) async {
+  final Uri phoneUri = Uri(scheme: 'tel', path: number);
+  if (await canLaunchUrl(phoneUri)) {
+    await launchUrl(phoneUri);
+  } else {
+    throw 'Could not launch $phoneUri';
+  }
+}
+
 class DetailsPrice extends StatelessWidget {
   const DetailsPrice({
     super.key,
   });
+
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +53,9 @@ class DetailsPrice extends StatelessWidget {
             Width: screenWidth * 0.45,
             Riduse: 12,
             Elevated: 0,
-            OnPressed: () {},
+            OnPressed: () async{
+             await makePhoneCall(phoneNumber);
+            },
             color: blue,
             borderSidecolor: blue,
             widget: Center(
@@ -53,4 +70,3 @@ class DetailsPrice extends StatelessWidget {
     );
   }
 }
-
